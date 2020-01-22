a14ced093ca9290d2a36a091375e05b395cdad2e
Vasyl <vsmentvs@gmail.com>
2020-01-22 19:58:46 +0200

    Topics are in Service class. Get all/specific topic, add topic.

--- src/main/java/pr/eleks/we_at_her/topic/TopicController.java
+++ src/main/java/pr/eleks/we_at_her/topic/TopicController.java
@@ -1,21 +1,29 @@
 package pr.eleks.we_at_her.topic;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class TopicController {
 
+    @Autowired
+    private TopicService topicService;
+
     @RequestMapping("/topics")
-    public List<Topic> getAllTopics(){
-        return Arrays.asList(
-                new Topic("spring","Spring Framework","Spring Framework Description"),
-                new Topic("autumn","Autumn Framework","Autumn Framework Description"),
-                new Topic("summer","Summer Framework","Summer Framework Description"),
-                new Topic("winter","Winter Framework","Winter Framework Description")
-        );
+    public List<Topic> getAllTopics() {
+        return topicService.getAllTopics();
     }
+
+    @RequestMapping("/topic/{id}")
+    public Topic getTopic(@PathVariable String id) {
+        return topicService.getTopic(id);
+    }
+
+    @RequestMapping(value = "/topics", method = RequestMethod.POST)
+    public void addTopic(@RequestBody Topic topic) {
+        topicService.addTopic(topic);
+    }
+
 }
---
+++ src/main/java/pr/eleks/we_at_her/topic/TopicService.java
@@ -0,0 +1,29 @@
+package pr.eleks.we_at_her.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+    List<Topic> topics = new ArrayList<>(Arrays.asList(
+            new Topic("winter", "Winter season", "Winter is the coldest season of the year in polar and temperate zones."),
+            new Topic("spring", "Spring season", "Spring, also known as springtime is one of the four temperate seasons, following winter and preceding summer."),
+            new Topic("summer", "Summer season", "Summer is the hottest of the four temperate seasons, falling after spring and before autumn."),
+            new Topic("autumn", "Autumn season", "Autumn, also known as fall in North American English, is one of the four temperate seasons.")
+    ));
+
+    public List<Topic> getAllTopics() {
+        return topics;
+    }
+
+    public Topic getTopic(String id) {
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+}

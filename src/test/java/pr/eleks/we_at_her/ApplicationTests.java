d5ae46f742d6db40f4b7a864b35b6bd9e7def6f5
Vasyl <vsmentvs@gmail.com>
2020-01-22 21:11:04 +0200

    some merge corrections

--- src/test/java/pr/eleks/we_at_her/ApplicationTests.java
+++ src/test/java/pr/eleks/we_at_her/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package pr.eleks.we_at_her;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {

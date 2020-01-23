3e20eff18b5a0df30b8db5826008a0cc592a0db7
Vasyl <vsmentvs@gmail.com>
2020-01-23 16:28:08 +0200

    hibernate support

--- create.sql
+++
@@ -1,6 +0,0 @@
-create table course (id varchar(255) not null, description varchar(255), name varchar(255), topic_id varchar(255), primary key (id))
-create table topic (id varchar(255) not null, description varchar(255), name varchar(255), primary key (id))
-alter table course add constraint FKokaxyfpv8p583w8yspapfb2ar foreign key (topic_id) references topic
-create table course (id varchar(255) not null, description varchar(255), name varchar(255), topic_id varchar(255), primary key (id))
-create table topic (id varchar(255) not null, description varchar(255), name varchar(255), primary key (id))
-alter table course add constraint FKokaxyfpv8p583w8yspapfb2ar foreign key (topic_id) references topic
--- src/main/java/pr/eleks/we_at_her/Application.java
+++ src/main/java/pr/eleks/we_at_her/Application.java
@@ -1,18 +1,55 @@
 package pr.eleks.we_at_her;
 
 import java.util.Arrays;
+import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import pr.eleks.we_at_her.hibernate.HibernateUtil;
+import pr.eleks.we_at_her.topic.Topic;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @PostConstruct
+    private static void init() {
+        Topic student = new Topic("Ramesssssssh", "Fadatare", "rameshfadatare@javaguides.com");
+        Topic student1 = new Topic("Ja sdad sd ohn", "Cena", "john@javaguides.com");
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the student objects
+            session.save(student);
+            session.save(student1);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            List<Topic> topics = session.createQuery("from Topic", Topic.class).list();
+            topics.forEach(t -> System.out.println(t.getName()));
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
 //    @Bean
@@ -29,8 +66,5 @@ public class Application {
 //
 //        };
 //    }
-//    git branch -m old_branch new_branch         # Rename branch locally
-//git push origin :old_branch                 # Delete the old branch
-//git push --set-upstream origin new_branch   # Push the new branch, set local branch to track the new remote
 
 }

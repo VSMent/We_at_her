fc748fc01b784f6186f2422f58e878ac5072da52
Vasyl <vsmentvs@gmail.com>
2020-01-24 19:13:40 +0200

    Complete remove of Topic sample class

--- src/main/java/pr/eleks/we_at_her/repositories/TopicRepository.java
+++
@@ -1,8 +0,0 @@
-package pr.eleks.we_at_her.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import pr.eleks.we_at_her.entities.Topic;
-
-public interface TopicRepository extends CrudRepository<Topic, String> {
-
-}

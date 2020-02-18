2ffb5dec3b5d228d9fb39e5888cdc438c74f6a1f
Vasyl <vsmentvs@gmail.com>
2020-02-18 19:01:20 +0200

    Add methods to interface, refactor code

--- src/main/java/pr/eleks/we_at_her/services/view/ViewService.java
+++ src/main/java/pr/eleks/we_at_her/services/view/ViewService.java
@@ -1,9 +1,15 @@
 package pr.eleks.we_at_her.services.view;
 
+import pr.eleks.we_at_her.dto.CityDto;
+import pr.eleks.we_at_her.dto.UserDto;
 import pr.eleks.we_at_her.dto.weather.WeatherSampleDto;
+import pr.eleks.we_at_her.exceptions.PropertyNotFoundException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface ViewService {
-    List<WeatherSampleDto> getAllWeatherSamples();
+    List<WeatherSampleDto> getAllWeatherSamples() throws PropertyNotFoundException;
+
+    UserDto createUser(UserDto userDto);
 }
--- src/main/java/pr/eleks/we_at_her/services/view/impl/ViewServiceImpl.java
+++ src/main/java/pr/eleks/we_at_her/services/view/impl/ViewServiceImpl.java
@@ -4,15 +4,19 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+import pr.eleks.we_at_her.dto.CityDto;
 import pr.eleks.we_at_her.dto.UserDto;
 import pr.eleks.we_at_her.dto.weather.WeatherSampleDto;
+import pr.eleks.we_at_her.exceptions.PropertyNotFoundException;
+import pr.eleks.we_at_her.services.view.ViewService;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public class ViewServiceImpl {
+public class ViewServiceImpl implements ViewService {
 
     private Environment env;
     private RestTemplate restTemplate;
@@ -22,23 +26,29 @@ public class ViewServiceImpl {
         this.restTemplate = restTemplate;
     }
 
-    public List<WeatherSampleDto> getAllWeatherSamples() {
-        String apiUrl = env.getProperty("server.host") + ":" + env.getProperty("server.port");
+    @Override
+    public List<WeatherSampleDto> getAllWeatherSamples() throws PropertyNotFoundException {
         UriComponentsBuilder uriBuilder = UriComponentsBuilder
-                .fromUriString(apiUrl)
-                .pathSegment("weatherSamplesREST");
+                .fromUriString(
+                        Optional.ofNullable(env.getProperty("server.host"))
+                                .orElseThrow(() -> new PropertyNotFoundException("server.host"))
+                                .concat(":").concat(
+                                Optional.ofNullable(env.getProperty("server.port"))
+                                        .orElseThrow(() -> new PropertyNotFoundException("server.port"))
+                        )
+                )
+                .pathSegment("weatherSampleREST");
 
         // Make request
-        WeatherSampleDto[] responseDtos = restTemplate.getForObject(uriBuilder.toUriString(), WeatherSampleDto[].class);
-        List<WeatherSampleDto> weatherSampleDtos;
-        if (responseDtos != null) {
-            weatherSampleDtos = new ArrayList<>(Arrays.asList(responseDtos));
-            return weatherSampleDtos;
-        } else {
-            return new ArrayList<>();
-        }
+
+        return new ArrayList<>(Arrays.asList(
+                Optional
+                        .ofNullable(restTemplate.getForObject(uriBuilder.toUriString(), WeatherSampleDto[].class))
+                        .orElse(new WeatherSampleDto[0])
+        ));
     }
 
+    @Override
     public UserDto createUser(UserDto userDto) {
         String apiUrl = env.getProperty("server.host") + ":" + env.getProperty("server.port");
         UriComponentsBuilder uriBuilder = UriComponentsBuilder

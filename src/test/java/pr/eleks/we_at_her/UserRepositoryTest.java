a1d714c4b822ae4ecb2ec4cfeaf52f34b5295ee7
Vasyl <vsmentvs@gmail.com>
2020-02-19 11:01:15 +0200

    Test user save_find

---
+++ src/test/java/pr/eleks/we_at_her/UserRepositoryTest.java
@@ -0,0 +1,65 @@
+package pr.eleks.we_at_her;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import pr.eleks.we_at_her.dto.RoleDto;
+import pr.eleks.we_at_her.entities.User;
+import pr.eleks.we_at_her.repositories.UserRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+//@AutoConfigureTestDatabase
+//@SpringBootTest
+//@Transactional
+//@ComponentScan(basePackages = {"pr.eleks.we_at_her"})
+//@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@ContextConfiguration(classes = {Application.class})
+public class UserRepositoryTest {
+
+//    @Autowired
+//    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void save_find() {
+        String username = "test";
+        String password = "user";
+        long cityId = 12345L;
+        String email = "EMaIL";
+        User user = new User();
+
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setCityId(cityId);
+        user.setEmail(email);
+        user.setRole(RoleDto.USER);
+
+        userRepository.save(user);
+
+        User foundUser = userRepository.findByUsername(username);
+
+        assertThat(user.getUsername())
+                .isNotBlank()
+                .isEqualTo(foundUser.getUsername());
+        assertThat(user.getPassword())
+                .isNotBlank()
+                .isEqualTo(foundUser.getPassword());
+        assertThat(user.getEmail())
+                .isNotBlank()
+                .isEqualTo(foundUser.getEmail());
+        assertThat(user.getCityId())
+                .isNotNull()
+                .isEqualTo(foundUser.getCityId());
+        assertThat(user.getRole())
+                .isEqualTo(foundUser.getRole());
+    }
+}
+

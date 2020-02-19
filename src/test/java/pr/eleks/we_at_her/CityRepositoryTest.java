a0c5f34b9f2f8fa223d9d77712830f753aaf83ea
Vasyl <vsmentvs@gmail.com>
2020-02-19 20:39:10 +0200

    Log Hibernate SQL Requests to console

--- src/test/java/pr/eleks/we_at_her/CityRepositoryTest.java
+++ src/test/java/pr/eleks/we_at_her/CityRepositoryTest.java
@@ -1,6 +1,8 @@
 package pr.eleks.we_at_her;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
@@ -13,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringJUnitConfig(Application.class)
 public class CityRepositoryTest {
 
+    private static Logger logger = LoggerFactory.getLogger(CityRepositoryTest.class);
     @Autowired
     private CityRepository cityRepository;
 

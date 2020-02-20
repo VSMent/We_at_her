dff7e22dd1031be4641f6a83968d5ebc495c6277
Vasyl <vsmentvs@gmail.com>
2020-02-20 14:33:03 +0200

    Return blogPosts in reverse order

--- src/main/java/pr/eleks/we_at_her/services/data/impl/BlogPostServiceImpl.java
+++ src/main/java/pr/eleks/we_at_her/services/data/impl/BlogPostServiceImpl.java
@@ -3,11 +3,11 @@ package pr.eleks.we_at_her.services.data.impl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
 import pr.eleks.we_at_her.dto.BlogPostDto;
-import pr.eleks.we_at_her.dto.CityDto;
 import pr.eleks.we_at_her.entities.BlogPost;
 import pr.eleks.we_at_her.repositories.BlogPostRepository;
 import pr.eleks.we_at_her.services.data.BlogPostService;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -30,8 +30,10 @@ public class BlogPostServiceImpl implements BlogPostService {
 
     @Override
     public List<BlogPostDto> findAllBlogPosts() {
-        return StreamSupport.stream(blogPostRepository.findAll().spliterator(), false)
+        List<BlogPostDto> blogPostDtos = StreamSupport.stream(blogPostRepository.findAll().spliterator(), false)
                 .map(this::convertToDto).collect(Collectors.toList());
+        Collections.reverse(blogPostDtos);
+        return blogPostDtos;
     }
 
     @Override
